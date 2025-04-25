e3b73ba403c5cd29e369c9c5bbbc248cbdd3e74c
Nurikcmd <mr.nursat04@gmail.com>
2025-04-25 12:34:31 +0500

    git

---
+++ src/test/java/org/example/my/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
